0808c75fbcb71016311f7fea29acea5e4f2db1fd
hugo <hugo@LAPTOP-81A7I661>
2025-07-23 12:04:48 +0800

    加入專案檔案。

---
+++ ProjectMVC/Controllers/ProjectController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectMVC.Models;
+
+namespace ProjectMVC.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ForProjectContext db = new ForProjectContext();
+        public IActionResult Index()
+        {
+            var projects = db.Projects.ToList();
+            return View(projects);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var project = db.Projects.FirstOrDefault(p => p.Id == id);
+            if (project == null)
+                return NotFound();
+            return View(project);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(Contacts contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contact.SubmitDate = DateTime.Now;
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(contact);
+        }
+    }
+}
